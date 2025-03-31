073dbcce3843aa721d66f883c4657189393fa2dc
Yar <mail@seredkin-pro.ru>
2025-04-01 00:34:12 +0500

    Выполнено ДЗ №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b ** 2 - 4 * a * c;
   
+  if (d > 0) {
+    const x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    const x2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    const x = -b / (2 * a);
+    arr.push(x);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let perInMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let paymentMonthly = creditBody * (perInMonth + (perInMonth / (((1 + perInMonth) ** countMonths) - 1)));
+  let totalAmount = paymentMonthly * countMonths;
+  totalAmount = Number(totalAmount.toFixed(2)); 
+  return totalAmount;
 }
