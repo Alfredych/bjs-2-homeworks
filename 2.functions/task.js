3e026c73762c58744099fdae557520687395af1c
Yar <mail@seredkin-pro.ru>
2025-04-11 16:48:41 +0500

    Внесены правки в ДЗ №2 (12)

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,9 @@
 // Задача 1
 //---------
 
-let arr = [1, 2, 3, -100, 10];
+let arr1 = [-99, 99, 10];
+let arr2 = [1, 2, 3, -100, 10];
+let arr3 = [5];
 
 function getArrayParams(...arr) {
   let min = Infinity;
@@ -28,14 +30,15 @@ function getArrayParams(...arr) {
   };
 }
 
-const result = getArrayParams(...arr);
-console.log(result);
+console.log(getArrayParams(...arr1));
+console.log(getArrayParams(...arr2));
+console.log(getArrayParams(...arr3));
 
 //---------------------------------------
 // Задача 2
 //---------
 
-arr = [10, 10, 11, 20, 10];
+let arr4 = [10, 10, 11, 20, 10];
 
 function summElementsWorker(arr) {
   if (arr.length == 0) {
@@ -47,10 +50,10 @@ function summElementsWorker(arr) {
   }
 }
 
-console.log(summElementsWorker(arr));
+console.log(summElementsWorker(arr4));
 
 function differenceMaxMinWorker(arr) {
-  if (arr.length === 0) {
+  if (arr.length == 0) {
     return 0;
   } else {
       let min = Infinity;
@@ -65,12 +68,14 @@ function differenceMaxMinWorker(arr) {
   }
 }
 
-console.log(differenceMaxMinWorker(arr));
+console.log(differenceMaxMinWorker(arr4));
 
-arr = [94, 51, 57, 41, 47, 66, 58, 10, 38, 17];
+let arr5 = [94, 51, 57, 41, 47, 66, 58, 10, 38, 17];
+let arr6 = [15, 97, 85, 64, 67, 10, 69, 40, 15, 35];
+let arr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function differenceEvenOddWorker(arr) {
-  if (arr.length === 0) {
+  if (arr.length == 0) {
     return 0;
   } else {
       let sumEvenElement = 0;
@@ -86,12 +91,12 @@ function differenceEvenOddWorker(arr) {
     }
 }
 
-console.log(differenceEvenOddWorker(arr));
-
-arr = [15, 97, 85, 64, 67, 10, 69, 40, 15, 35];
+console.log(differenceEvenOddWorker(arr7));
+console.log(differenceEvenOddWorker(arr5));
+console.log(differenceEvenOddWorker(arr6));
 
 function averageEvenElementsWorker(arr) {
-  if (arr.length === 0) {
+  if (arr.length == 0) {
     return 0;
   } else {
     let sumEvenElement = 0;
@@ -106,13 +111,14 @@ function averageEvenElementsWorker(arr) {
   }
 }
 
-console.log(averageEvenElementsWorker(arr));
+console.log(averageEvenElementsWorker(arr7));
+console.log(averageEvenElementsWorker(arr6));
 
 //------------------------------------------
 // Задача 3
 //---------
 
-arr = [
+let arr8 = [
   [10, 10, 11, 20, 10], 
   [67, 10, 2, 39, 88], 
   [72, 75, 51, 87, 43], 
@@ -130,7 +136,7 @@ function makeWork(arrOfArr, func) {
   return maxWorkerResult;
 }
 
-console.log(makeWork(arr, summElementsWorker));
-console.log(makeWork(arr, differenceMaxMinWorker));
-console.log(makeWork(arr, differenceEvenOddWorker));
-console.log(makeWork(arr, averageEvenElementsWorker));
+console.log(makeWork(arr8, summElementsWorker));
+console.log(makeWork(arr8, differenceMaxMinWorker));
+console.log(makeWork(arr8, differenceEvenOddWorker));
+console.log(makeWork(arr8, averageEvenElementsWorker));
