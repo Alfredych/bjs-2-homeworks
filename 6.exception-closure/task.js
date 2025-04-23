3a9da86bf145dec1f263b4914383254582fba1af
Yar <mail@seredkin-pro.ru>
2025-04-24 00:05:41 +0500

    Сделано ДЗ №6 (Обработка исключений и замыкания)

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,61 @@
-﻿
+﻿//--------------------------
+// Задача 1. Форматтер чисел
+//--------------------------
+
+function parseCount(number) {
+  const parsedNamber = Number.parseFloat(number);
+  if (isNaN(parsedNamber)) {
+    throw new Error("Невалидное значение")
+  }
+  return parsedNamber;
+}
+
+function validateCount(number) {
+  try {
+    return parseCount(number);
+  } catch (error) {
+    return error;
+  }
+}
+
+
+//----------------------
+// Задача 2. Треугольник
+//----------------------
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this._perimeter = this.a + this.b + this.c;    
+    const pp = this._perimeter / 2; // полупериметр для формулы Герона
+    this._area = Number((Math.sqrt(pp * (pp - this.a) * (pp - this.b) * (pp - this.c))).toFixed(3));
+    if (this.a + this.b <= this.c || this.a + this.c <= this.b || this.b + this.c <= this.a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+  
+  get perimeter() {
+    return this._perimeter;
+  }
+
+  get area() {
+    return this._area;
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
